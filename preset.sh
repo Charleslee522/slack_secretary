344e95ba41b8808e24e233bebf60321b1a1eadb4
charles <charleslee522@gmail.com>
2017-10-27 17:58:21 +0900

    add slack bot name environment variable

--- preset.sh
+++ preset.sh
@@ -1,7 +1,8 @@
 export SLACK_BOT_TOKEN='slack bot token'
+export SLACK_BOT_NAME='slack bot name'
 
 echo SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN
-echo SLACK_BOT_NAME='echooooo'
+echo SLACK_BOT_NAME=$SLACK_BOT_NAME
 
 source env_slack/bin/activate
 echo activate starterbot
